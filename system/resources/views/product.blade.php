adc3752a6506599c693ec9f973e18ea02d577ecf
Erwin0109 <71189288+Erwin0109@users.noreply.github.com>
2020-10-28 17:37:46 +0700

    Initial commit

---
+++ system/resources/views/product.blade.php
@@ -0,0 +1,93 @@
+@extends('template.base')
+
+@section('content')
+	<div class="brand_color">
+	    <div class="container">
+	        <div class="row">
+	            <div class="col-md-12">
+	                <div class="titlepage">
+	                    <h2>Product</h2>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	</div>
+
+	 <!-- brand -->
+	<div class="brand-bg">
+	    <div class="container">
+	        <div class="row">
+	            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 margin">
+	                <div class="brand_box">
+	                    <img src="{{url('public')}}/images/1.png" alt="img" />
+	                    <h3>Rp.<strong class="red">1.000.000</strong></h3>
+	                    <span>Mobile Phone</span>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                </div>
+	            </div>
+	            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 margin">
+	                <div class="brand_box">
+	                    <img src="{{url('public')}}/images/2.png" alt="img" />
+	                    <h3>Rp.<strong class="red">1.000.000</strong></h3>
+	                    <span>Mobile Phone</span>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                </div>
+	            </div>
+	            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 margin">
+	                <div class="brand_box">
+	                    <img src="{{url('public')}}/images/3.png" alt="img" />
+	                    <h3>Rp.<strong class="red">1.000.000</strong></h3>
+	                    <span>Mobile Phone</span>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                </div>
+	            </div>
+	            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6">
+	                <div class="brand_box">
+	                    <img src="{{url('public')}}/images/4.png" alt="img" />
+	                    <h3>Rp.<strong class="red">1.000.000</strong></h3>
+	                    <span>Mobile Phone</span>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                </div>
+	            </div>
+	            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 mrgn">
+	                <div class="brand_box">
+	                    <img src="{{url('public')}}/images/5.png" alt="img" />
+	                    <h3>Rp.<strong class="red">1.000.000</strong></h3>
+	                    <span>Mobile Phone</span>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                </div>
+	            </div>
+	            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 mrgn">
+	                <div class="brand_box">
+	                    <img src="{{url('public')}}/images/6.png" alt="img" />
+	                    <h3>Rp.<strong class="red">1.000.000</strong></h3>
+	                    <span>Mobile Phone</span>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                    <i><img src="{{url('public')}}/images/star.png"/></i>
+	                </div>
+	            </div>
+	            <div class="col-md-12">
+	                <a class="read-more">See More</a>
+	            </div>
+	        </div>
+	    </div>
+	</div>
+	</div>
+@endsection
